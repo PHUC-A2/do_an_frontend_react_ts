3f3cfbbb37df28038a72ae646d73013bcc8b2a95
PHUC-A2 <phucban297@gmail.com>
2026-01-12 15:07:22 +0700

    #19 fix(auth): refresh access token on app init

--- src/hooks/init/useAuthInit.ts
+++ src/hooks/init/useAuthInit.ts
@@ -1,14 +1,28 @@
 import { useEffect } from "react";
 import { useAppDispatch } from "../../redux/hooks";
-import { setToken } from "../../redux/features/authSlice";
+import { setToken, setLogout } from "../../redux/features/authSlice";
+import axios from "axios";
 
 export const useAuthInit = () => {
     const dispatch = useAppDispatch();
 
     useEffect(() => {
-        const token = localStorage.getItem('access_token');
-        if (token) {
-            dispatch(setToken(token));
-        }
+        const initAuth = async () => {
+            try {
+                const res = await axios.get(
+                    `${import.meta.env.VITE_BACKEND_URL}/api/v1/auth/refresh`,
+                    { withCredentials: true }
+                );
+
+                const token = res.data?.data?.access_token;
+                if (token) {
+                    dispatch(setToken(token));
+                }
+            } catch {
+                dispatch(setLogout());
+            }
+        };
+
+        initAuth();
     }, [dispatch]);
-}
+};
