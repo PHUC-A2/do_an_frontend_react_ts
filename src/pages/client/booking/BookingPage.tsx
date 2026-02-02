91dfba9183395b4344c4f6014839b2637657dda3
PHUC-A2 <phucban297@gmail.com>
2026-02-02 17:52:17 +0700

    #01 components BookingPage

--- src/pages/client/booking/BookingPage.tsx
+++ src/pages/client/booking/BookingPage.tsx
@@ -4,28 +4,20 @@ import {
     DatePicker,
     Row,
     Typography,
-    Tag,
     Space,
     message,
     Spin,
-    Form,
-    Select,
-    Input,
-    Button
 } from "antd";
 import dayjs, { Dayjs } from "dayjs";
 import { useEffect, useState } from "react";
 import "./BookingPage.scss";
 import { PiSoccerBallFill } from "react-icons/pi";
-import { IoMdClock } from "react-icons/io";
 import { useParams } from "react-router";
 import type { IPitchTimeline } from "../../../types/timeline";
-import { createBookingClient, getPitchById, getTimeline } from "../../../config/Api";
-import type { ICreateBookingClientReq, ShirtOptionEnum } from "../../../types/booking";
-import { SHIRT_OPTION_OPTIONS } from "../../../utils/constants/booking.constants";
-import { toast } from "react-toastify";
-import { formatDateTime } from "../../../utils/format/localdatetime";
+import { getPitchById, getTimeline } from "../../../config/Api";
 import type { IPitch } from "../../../types/pitch";
+import BookingTime from "./components/BookingTimeline";
+import CreateBookingForm from "./components/CreateBookingForm";
 
 const { Title, Text } = Typography;
 
@@ -33,44 +25,17 @@ interface BookingPageProps {
     theme: "light" | "dark";
 }
 
-type BookingFormValues = {
-    userId: number;
-    pitchId: number;
-    shirtOption: ShirtOptionEnum;
-    contactPhone?: string;
-    dateTimeRange: [Dayjs, Dayjs];
-};
+
 
 const BookingPage: React.FC<BookingPageProps> = ({ theme }) => {
     const isDark = theme === "dark";
     const { pitchId } = useParams<{ pitchId: string }>();
     const pitchIdNumber = Number(pitchId);
-
     const [bookingDate, setBookingDate] = useState<Dayjs | null>(dayjs());
     const [timeline, setTimeline] = useState<IPitchTimeline | null>(null);
     const [timelineLoading, setTimelineLoading] = useState(false);
-    const [bookingLoading, setBookingLoading] = useState(false);
-    const [form] = Form.useForm<BookingFormValues>();
     const [pitch, setPitch] = useState<IPitch | null>(null);
     const [pitchLoading, setPitchLoading] = useState(false);
-    const dateTimeRange = Form.useWatch("dateTimeRange", form);
-    const shirtOption = Form.useWatch("shirtOption", form);
-
-    const previewPrice = (() => {
-        if (!dateTimeRange || !pitch) return 0;
-
-        const [start, end] = dateTimeRange;
-        const minutes = end.diff(start, "minute");
-        if (minutes <= 0) return 0;
-
-        let total = (pitch.pricePerHour / 60) * minutes;
-
-        if (shirtOption === "WITH_PITCH_SHIRT") {
-            total += 25000;
-        }
-
-        return Math.round(total);
-    })();
 
     useEffect(() => {
         if (!pitchIdNumber) return;
@@ -97,47 +62,6 @@ const BookingPage: React.FC<BookingPageProps> = ({ theme }) => {
     }, [bookingDate, pitchIdNumber]);
 
 
-    const handleBooking = async (values: BookingFormValues) => {
-        setBookingLoading(true);
-        const [start, end] = values.dateTimeRange;
-
-        const payload: ICreateBookingClientReq = {
-            pitchId: pitchIdNumber,
-            shirtOption: values.shirtOption,
-            contactPhone: values.contactPhone,
-            startDateTime: start.format("YYYY-MM-DDTHH:mm:ss"),
-            endDateTime: end.format("YYYY-MM-DDTHH:mm:ss"),
-        };
-
-        try {
-            const res = await createBookingClient(payload);
-            if (res.data.statusCode === 201) {
-                toast.success("Đặt sân thành công");
-                form.resetFields();
-                form.setFieldsValue({
-                    dateTimeRange: undefined,
-                    shirtOption: undefined,
-                });
-            }
-            // refresh timeline
-            getTimeline(pitchIdNumber, bookingDate!.format("YYYY-MM-DD"))
-                .then(res => {
-                    if (res.data.statusCode === 200) {
-                        setTimeline(res.data.data ?? null)
-                    }
-                });
-        } catch (e: any) {
-            const m = e?.response?.data?.message ?? "Khung giờ không hợp lệ";
-            toast.error(
-                <div>
-                    <div><strong>Có lỗi xảy ra!</strong></div>
-                    <div>{m}</div>
-                </div>
-            );
-        } finally {
-            setBookingLoading(false);
-        }
-    };
 
     const formatTime = (time?: string) =>
         time ? dayjs(`1970-01-01T${time}`).format("HH:mm") : "--:--";
@@ -158,45 +82,10 @@ const BookingPage: React.FC<BookingPageProps> = ({ theme }) => {
 
                     {/* ===== LEFT: TIMELINE ===== */}
                     <Col xs={24} lg={16}>
-                        <Title level={5}>
-                            <Space>
-                                <IoMdClock size={22} />
-                                <span>Timeline</span>
-                            </Space>
-                        </Title>
-
-                        <Space style={{ marginBottom: 12 }}>
-                            <Tag color="green">Trống</Tag>
-                            <Tag color="red">Đã đặt</Tag>
-                        </Space>
-
-                        {timelineLoading ? (
-                            <Spin />
-                        ) : (
-                            <div className="time-grid-wrapper">
-                                <div className="time-grid">
-                                    {timeline?.slots.map(slot => {
-                                        const isBusy = slot.status === "BUSY";
-
-                                        return (
-                                            <div
-                                                key={slot.start}
-                                                className={`time-slot luxury ${isBusy ? "booked" : "free"}`}
-                                            >
-                                                <div className="slot-inner">
-                                                    <div className="time">
-                                                        {formatDateTime(slot.start, "HH:mm")}
-                                                    </div>
-                                                    <div className="label">
-                                                        {isBusy ? "ĐÃ ĐẶT" : "TRỐNG"}
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        );
-                                    })}
-                                </div>
-                            </div>
-                        )}
+                        <BookingTime
+                            timelineLoading={timelineLoading}
+                            timeline={timeline}
+                        />
                     </Col>
 
                     {/* ===== RIGHT: INFO ===== */}
@@ -249,86 +138,14 @@ const BookingPage: React.FC<BookingPageProps> = ({ theme }) => {
                             )
                         )}
 
-                        {/* sdas */}
-                        <Form
-                            form={form}
-                            layout="vertical"
-                            onFinish={handleBooking}
-                            style={{ marginTop: 24 }}
-                        >
-
-
-                            <Form.Item
-                                label="Thời gian đặt sân"
-                                name="dateTimeRange"
-                                rules={[{ required: true, message: "Vui lòng chọn thời gian" }]}
-                            >
-                                <DatePicker.RangePicker
-                                    showTime={{ format: "HH:mm" }}
-                                    format="YYYY-MM-DD HH:mm"
-                                    style={{ width: "100%" }}
-                                    minuteStep={5}
-                                    placeholder={["Thời gian bắt đầu", "Thời gian kết thúc"]}
-                                    disabledDate={d => d.isBefore(dayjs().startOf("day"))}
-                                />
-                            </Form.Item>
-
-                            {pitchLoading ? (
-                                <Spin />
-                            ) : (
-                                pitch && dateTimeRange && (
-                                    <Card size="small" style={{ marginBottom: 16 }}>
-                                        {dateTimeRange ? (
-                                            <>
-                                                <Text>
-                                                    ⏱ Thời lượng: {dateTimeRange[1].diff(dateTimeRange[0], "minute")} phút
-                                                </Text>
-                                                <br />
-
-                                                {shirtOption === "WITH_PITCH_SHIRT" && (
-                                                    <>
-                                                        <Text>👕 Áo pitch: +25.000 đ</Text>
-                                                        <br />
-                                                    </>
-                                                )}
-
-                                                <Text strong style={{ fontSize: 18, color: "#22c55e" }}>
-                                                    💰 Tạm tính: {previewPrice.toLocaleString("vi-VN")} đ
-                                                </Text>
-                                            </>
-                                        ) : (
-                                            <Text type="secondary">
-                                                Chọn thời gian để xem giá 💡
-                                            </Text>
-                                        )}
-                                    </Card>
-                                )
-                            )}
-
-                            <Form.Item
-                                label="Áo pitch"
-                                name="shirtOption"
-                                rules={[{ required: true }]}
-                            >
-                                <Select options={SHIRT_OPTION_OPTIONS} />
-                            </Form.Item>
-
-                            <Form.Item label="Số điện thoại" name="contactPhone">
-                                <Input />
-                            </Form.Item>
-
-                            <Button
-                                type="primary"
-                                block
-                                loading={bookingLoading}
-                                htmlType="submit"
-                                disabled={!dateTimeRange || !shirtOption}
-                            >
-                                Đặt sân
-                            </Button>
-
-                        </Form>
-
+                        {/* Create */}
+                        <CreateBookingForm
+                            pitchIdNumber={pitchIdNumber}
+                            setTimeline={setTimeline}
+                            bookingDate={bookingDate}
+                            pitch={pitch}
+                            pitchLoading={pitchLoading}
+                        />
                     </Col>
 
                 </Row>
