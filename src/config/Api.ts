d75a8124074aeb2f19e77c15256fee47dfb23704
PHUC-A2 <phucban297@gmail.com>
2026-02-04 00:52:08 +0700

    #57 (fix) remove access_token

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -13,7 +13,16 @@ import instance from "./customAxios";
 
 export const register = (data: IRegister) => instance.post("/api/v1/auth/register", data);
 export const login = (username: string, password: string) => instance.post("/api/v1/auth/login", { username, password });
-export const logout = () => instance.post("/api/v1/auth/logout");
+// export const logout = () => instance.post("/api/v1/auth/logout");
+export const logout = async () => {
+    try {
+        const res = await instance.post("/api/v1/auth/logout");
+        return res;
+    } finally {
+        localStorage.removeItem("access_token");
+        // store.dispatch(setLogout());
+    }
+};
 export const getAccount = () => instance.get("/api/v1/auth/account");
 export const getRefreshToken = () => instance.get("/api/v1/auth/refresh");
 
